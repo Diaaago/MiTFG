8a3b9897cc0b788100b0fa8ad651412045617f20
JunjieLi <dshen123456@gmail.com>
2022-07-12 20:05:21 +0200

    menu

--- frontend/src/componente/Foods.js
+++ frontend/src/componente/Foods.js
@@ -4,19 +4,35 @@ export default class Foods extends Component {
   constructor(props){
       super(props);
       this.state = {
-        productos : []
-        
+        productos : [],
+        id: ''
       }
   }
   
   async componentDidMount() {
-    await axios.get('http://localhost:4000/food').then(response => {
+    /*await axios.get('http://localhost:4000/food').then(response => {
       this.setState({productos: response.data});
+    })*/
+
+    //this.getfood(this.state.id);
+    console.log('data', this)
+  }
+
+  getfood = async (id) => {
+    await axios.get('http://localhost:4000/food/' + id ).then(response => {
+    this.setState({productos: response.data})
     })
+  }
 
-    //const producto = await axios.get('http://localhost:4000/food/8410368032056');
-    //console.log('productos',this.state.productos);
+  obtenerId = (e) => {
+    this.setState({
+      id : e.target.value
+    })
+  }
 
+  buscar = async (e) => {
+    e.preventDefault ();
+    this.getfood(this.state.id);
   }
 
   render() {
@@ -25,13 +41,17 @@ export default class Foods extends Component {
     const imgURL = this.state.productos.map(food => food.img);
     return (
       <>
-      <div></div>
-        <img src = {imgURL}/>
-        <div>url: {this.state.productos.map(food => food.img)}</div>
-        <div>nombre del producto: {this.state.productos.map(food => food.product_name)}</div>
-        <div>marca: {this.state.productos.map(food => food.brand)}</div>
-        <div>etiquetas: {this.state.productos.map(food => food.etiquetas)}</div>
-        <div>paises de venta: {this.state.productos.map(food => food.country)}</div>
+        <div>
+          <form onSubmit={this.buscar}>
+            <input type="text" onChange={this.obtenerId}/>
+            <button type="submit">buscar</button>
+          </form>
+        </div>
+          <img src = {imgURL}/>
+          <div>nombre del producto: {this.state.productos.map(food => food.product_name)}</div>
+          <div>marca: {this.state.productos.map(food => food.brand)}</div>
+          <div>etiquetas: {this.state.productos.map(food => food.etiquetas)}</div>
+          <div>paises de venta: {this.state.productos.map(food => food.country)}</div>
       </>
     )
   }
