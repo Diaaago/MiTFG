fde441d5740df2c959e5b539eb339f4b8d43b6bf
JunjieLi <dshen123456@gmail.com>
2022-08-10 15:47:46 +0200

    csv

--- frontend/src/componente/produccion.js
+++ frontend/src/componente/produccion.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
+import * as d3 from "d3";
+
 export default class produccion extends Component {
     constructor(props){
         super(props);
         this.state = {
-          datosProducion: []
+          eco2: []
         }
     }
-    componentDidMount() { this.readText() }
+    componentDidMount() { 
+        //this.readText() 
+        d3.csv('./nodo1.csv', function(csvdata){
+            for(let i = 0; i < 5; i++) {
+                console.log('eco2', csvdata[i].eco2)
+                
+                /*let eco2 = csvdata[i].eco2;
+                let humedad = csvdata[i].humedad;
+                let temperatura = csvdata[i].temperatura;
+                let tvoc = csvdata[i].tvoc;*/
+            }
+        
+        })
+        console.log('this', this)
+    }
 
-    readText() {
+    /*readText() {
         fetch('./nodo1.csv').then(response => response.text()).then(responseText => {
             this.setState({
                 datosProducion: responseText
             })
         })
-    }
+    }*/
 
 
     render() {
         return (
-        <div>{this.state.datosProducion}</div>
+        <div></div>
         )
     }
 }
