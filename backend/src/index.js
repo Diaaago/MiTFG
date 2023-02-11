eac7751be822b0c984f78d7446a3b5f0afc8f45d
JunjieLi <dshen123456@gmail.com>
2023-02-11 17:14:29 +0100

    cambiar estructura

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
 const app = require('./app');
 require('./conexion');
 
-async function main(){
+async function main() {
     await app.listen(app.get('port'));
-    console.log('Server is running on port 4000');
+    console.log('Server is running on port ' + app.get('port'));
 }
 
 main();
