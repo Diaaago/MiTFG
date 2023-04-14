4e77629a2ca3097e7d384ebf363db051f00bc0be
Junjie <dshen123456@gmail.com>
2023-04-14 16:33:51 +0200

    fix

--- backend/src/conexion.js
+++ backend/src/conexion.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 //const { createIndexes } = require('./models/votModel');
 
 
-mongoose.connect('mongodb://localhost:27017/openFood', {
+mongoose.connect('mongodb://127.0.0.1:27017/openFood', {
     useNewUrlParser: true
 }).catch(err => console.log('error', err));
 
